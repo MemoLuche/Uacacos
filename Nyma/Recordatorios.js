9fe4233811261f657383451671a86cadba23df5b
MemoLuche <guillermogp08@gmail.com>
2025-03-29 03:04:57 -0600

    Cuerpo de paginas nuevas

---
+++ Nyma/Recordatorios.js
@@ -0,0 +1,25 @@
+// Seguridad.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Seguridad() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Recordatorios</Text>
+      <Text>Bienvenido al apartado de Recordatorios de NYMA</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F9F8F6',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
